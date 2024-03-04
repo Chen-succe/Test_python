69c8a38b4b0c72e8f7d614fdb52083e749fd5d93
Chen-succe <482049605@qq.com>
2024-03-04 15:51:51 +0800

    增加几个正则的方法，match、search，findall

--- Test_python/muzhou/字符串/zhengze.py
+++ Test_python/muzhou/字符串/zhengze.py
@@ -2,10 +2,10 @@
 # 导入
 import re
 
-pattern = '\d\.\d+'  # 第一个\d是匹配整数，第二个转义字符加.就是.的意思，
+pattern = '\d\.\d+'  # 第一个\d是匹配整数，第二个转义字符加.就是.的意思，(即匹配所有数字.数字形式，如2.3)
 # 第三个\d后面加一个+号限定符表示限定次数，可以多次
 
-s = 'I study python 3.10 every day'  # 待匹配的字符串
+s = 'I study python 3.10 every day, 0.5'  # 待匹配的字符串
 
 match = re.match(pattern, s, re.I)  # re.I 表示不区分大小写 I表示ignore，但是这
 # 里匹配pattern中只匹配了整数，整数是没有大小写的，实际上这里的match方法中第三个参数不起作用。
@@ -23,3 +23,9 @@ print('匹配值的结束位置：', match2.end())
 print('匹配值的位置元祖：', match2.span())
 print('待匹配的字符串：', match2.string)  # 注意，这个不是方法
 print('匹配的数据：', match2.group())
+
+search = re.search(pattern, s, re.I)
+print(search.group())
+
+findall = re.findall(pattern, s, re.I)
+print(findall)
